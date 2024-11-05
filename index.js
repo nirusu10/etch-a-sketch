d05517e5128824b47da795014e46031048d8e1a4
Nils Matic <matic1909@gmail.com>
2024-11-05 20:56:30 +0100

    added button to generate new grid with custom size

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('#grid-container')
+const newGridButton = document.querySelector('#new-grid-button')
 
 let mousePressed = false
 
@@ -10,14 +11,31 @@ document.addEventListener('mouseup', () => {
   mousePressed = false
 })
 
-for (let i = 0; i < 16 * 16; i++) {
-  const cell = document.createElement('div')
-  cell.classList.add('cell')
-  container.appendChild(cell)
-  cell.addEventListener('mouseover', () => {
-    if (mousePressed) {
-      cell.classList.add('filled')
-    }
-  })
-  cell.addEventListener('mousedown', () => cell.classList.add('filled'))
+newGridButton.addEventListener('click', () => {
+  let size
+
+  do {
+    size = parseInt(prompt('How many cells wide should your grid be?'))
+  } while (Number.isNaN(Number(size)))
+  generateGrid(size)
+})
+
+function generateGrid(size) {
+  container.innerHTML = ''
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement('div')
+    cell.style.cssText = `width: ${800 / size}px; height: ${
+      800 / size
+    }px; border: 1px solid gray; user-select: none`
+
+    container.appendChild(cell)
+    cell.addEventListener('mouseover', () => {
+      if (mousePressed) {
+        cell.classList.add('filled')
+      }
+    })
+    cell.addEventListener('mousedown', () => cell.classList.add('filled'))
+  }
 }
+
+generateGrid(16)
